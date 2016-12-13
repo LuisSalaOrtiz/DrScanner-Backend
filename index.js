155cea747ccdc4d57920baf58a03ca0a3cc7bc13
Luis Sala <albertitoclick@hotmail.com>
2016-12-13 04:25:37 -0400

    testing7

--- index.js
+++ index.js
@@ -22,15 +22,15 @@ app.get('/db', function (request, response) {
     client.query('SELECT * FROM test_table', function(err, result) {
       done();
       if (err)
-       { console.error(err); response.send("Error " + err); }
+      { console.error(err); response.send("Error " + err); }
       else
-       {
+      {
 
         //  response.render('pages/db', {results: result.rows} );
-          response.json(result.rows);
-          console.log(result.rows);
+        response.json(result.rows);
+        console.log(result.rows);
 
-         }
+      }
     });
   });
 });
@@ -40,15 +40,15 @@ app.get('/users/:email/', function (request, response) {
     client.query(('SELECT email,password,type FROM users WHERE email=\'').concat(request.params.email.concat("\'")), function(err, result) {
       done();
       if (err)
-       { console.error(err); response.send("Error " + err); }
+      { console.error(err); response.send("Error " + err); }
       else
-       {
+      {
 
         //  response.render('pages/db', {results: result.rows} );
-          response.json(result.rows);
-          console.log(result.rows);
+        response.json(result.rows);
+        console.log(result.rows);
 
-         }
+      }
     });
   });
 });
@@ -58,35 +58,32 @@ app.get('/patients/:qrcode/', function (request, response) {
     client.query(('select qrcode, patient.pid, pfirst, plast, ssn, birth, email, marital, gender, phone, weight, height, blood, address, hcname, hcnum, hcexp, vdate, vid, dfirst, dlast, specialty, cname, severity from (patient natural inner join (personal_info natural left join address natural left join healthcare) natural left join ((visits natural left join doctor) natural left join diagnostic natural left join condition)) where patient.qrcode=\'').concat(request.params.qrcode.concat("\'")), function(err, result) {
       done();
       if (err)
-       { console.error(err); response.send("Error " + err); }
+      { console.error(err); response.send("Error " + err); }
       else
-       {
-          response.json(result.rows);
-          console.log(result.rows);
+      {
+        response.json(result.rows);
+        console.log(result.rows);
 
-         }
+      }
     });
   });
 });
 
-// app.get('/post/user/:password/:type/:email/',function(request, response) {
-//   const pass = request.params.password;
-//   const type = request.params.type;
-//   const email = request.params.email;
-//   pg.connect(url, function(err, client, done) {
-//     client.query('insert into users (password, type, email) values (\'$1\', \'$2\', \'$3\')', [pass, type, email]), function(err, result) {
-//       done();
-//       if (err)
-//        { console.error(err); response.send("Error " + err); }
-//       else
-//        {
-//           response.json(result.rows);
-//           console.log(result.rows);
-//
-//          }
-//     });
-//   });
-// });
+app.post('/post/user/:password/:type/:email/',function(request, response) {
+  // const pass = request.params.password;
+  // const type = request.params.type;
+  // const mail = request.params.email;
+  const data = {pass: request.params.password, type: request.params.type, mail: request.params.email};
+  console.log(data);
+  pg.connect(url, function(err, client, done) {
+    client.query('insert into users (password, type, email) values ($1, $2, $3)', [data.pass, data.type, data.mail]);
+    if(err) {
+      done();
+      console.log(err);
+      return response.status(500).json({success: false, data: err});
+    }
+  });
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));

6db11e853961e64a7c5285575fb2f2582b380f47
Luis Sala <albertitoclick@hotmail.com>
2016-12-16 10:04:50 -0400

    Testing

--- index.js
+++ index.js
@@ -2,76 +2,28 @@ var express = require('express');
 const router = express.Router();
 var app = express();
 
-var allowCrossDomain = function(req, res, next) {
-
-
-
-  res.header('Access-Control-Allow-Origin', '*');
-
-
-
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-
-
-
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-
-
-
-
-
-
-
-
-  // intercept OPTIONS method
-
-
-
-  if ('OPTIONS' == req.method) {
-
-
-
-    res.send(200);
-
-
-
-  }
-
-
-
-  else {
-
-
-
-    next();
-
-
-
-  }
-
-
-};
-
-
-
-
-
-
-
-
-
-app.configure(function () {
-
-
-
-  app.use(allowCrossDomain);
-
-
-
-});
-
+// var allowCrossDomain = function(req, res, next) {
+//
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+// // intercept OPTIONS method
+//   if ('OPTIONS' == req.method) {
+//     res.send(200);
+//   }
+//   else {
+//     next();
+//   }
+// };
+//
+// app.configure(function () {
+//   app.use(allowCrossDomain);
+// });
+//
 var bodyParser = require('body-parser');
+// app.use(express.bodyParser());
+
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var pg = require('pg');

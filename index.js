6f45d8162f17547a0b5b182fd67a9c17691a3d56
Luis Sala <albertitoclick@hotmail.com>
2017-01-24 01:16:27 -0400

    App Completed.

--- index.js
+++ index.js
@@ -185,6 +185,72 @@ app.post('/post/patient/', function(request, response) {
   });
 });
 //_______________________________________________________________________________________________________________________
+
+//Update a patient info
+//_______________________________________________________________________________________________________________________
+app.PUT('/update/patient/', function(request, response) {
+  const patientData = {qr: request.body.qrcode, pf: request.body.pfirst, pl: request.body.plast, ssn: request.body.ssn};
+  const addressData = {address: request.body.address}
+  const healthData = {hcname: request.body.hcname, hcnum: request.body.hcnum};
+  const personalData = {email: request.body.email, marital: request.body.marital, gender: request.body.gender, phone: request.body.phone, weight: request.body.weight, height: request.body.height, blood: request.body.blood, age: request.body.age};
+  const numberOfCon = request.body.number;
+
+  var queryString = 'with pat as (update patient set pfirst=$1, plast=$2, ssn=$3 where qrcode=$4 returning pid), per_info as (update personal_info set email=$5, marital=$6, gender=$7, weight=$8, height=$9, blood=$10, age=$11, phone=$12 where pid=(select pid from pat) returning aid, hcid), addr as (update address set address=$13 where aid=(select aid from per_info)), health as (update healthcare set hcname=$14, hcnum=$15 where hcid=(select hcid from per_info)) insert into condition (diagid, cname, severity) values ';
+  //((select diagid from patient, visits, diagnostic where patient.pid=(select pid from pat)), $16, 'High')';
+
+  var listOfElements = [patientData.pf, patientData.pl, patientData.ssn, patientData.qr, personalData.email, personalData.marital, personalData.gender, personalData.weight, personalData.height, personalData.blood, personalData.age, personalData.phone, addressData.address, healthData.hcname, healthData.hcnum];
+
+  //((select diagid from diag), 'Muerte', 'High'), ((select diagid from diag), 'Vida', 'High')'
+
+  for(var i=0; i<numberOfCon; i++)
+  {
+    var identifier = '$' + (16+i);
+    queryString += '(((select diagid from patient, visits, diagnostic where patient.pid=(select pid from pat)), '+identifier+', \'High\')';
+    listOfElements.push(request.body['cname'+(i+1)]);
+    if(numberOfCon>(i+1))
+    {
+      queryString += ', '
+    }
+  }
+
+  console.log('Post patient');
+  pg.connect(url, function(err, client, done) {
+
+    client.query(queryString, listOfElements, function(err, result) {
+
+      if(err) {
+        done();
+        console.log(err);
+        return response.status(500).json({success: false, data: err});
+      }
+      else
+      {
+        response.json({Process: 'Complete', Status: 'Succesful'});
+        console.log(result.rows);
+      }
+    });
+  });
+
+  console.log('Delete duplicates');
+  pg.connect(url, function(err, client, done) {
+
+    client.query('delete from condition where exists (select 1 from condition as t2 where t2.cname = condition.cname and t2.diagid = condition.diagid and t2.cid > condition.cid)', function(err, result) {
+
+      if(err) {
+        done();
+        console.log(err);
+        return response.status(500).json({success: false, data: err});
+      }
+      else
+      {
+        response.json({Delete_Duplicates: 'Complete', Status: 'Succesful'});
+        console.log(result.rows);
+      }
+    });
+  });
+});
+//_______________________________________________________________________________________________________________________
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });

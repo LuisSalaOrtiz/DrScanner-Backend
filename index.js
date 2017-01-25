e2496fb066960ed52c6fc8a1fa4d2f0f63364bc0
Luis Sala <albertitoclick@hotmail.com>
2017-01-24 22:02:46 -0400

    Testing23

--- index.js
+++ index.js
@@ -195,7 +195,14 @@ app.put('/update/patient/', function(request, response) {
   const personalData = {email: request.body.email, marital: request.body.marital, gender: request.body.gender, phone: request.body.phone, weight: request.body.weight, height: request.body.height, blood: request.body.blood, age: request.body.age};
   const numberOfCon = request.body.number;
 
-  var queryString = 'with pat as (update patient set pfirst=$1, plast=$2, ssn=$3 where qrcode=$4 returning pid), per_info as (update personal_info set email=$5, marital=$6, gender=$7, weight=$8, height=$9, blood=$10, age=$11, phone=$12 where pid=(select pid from pat) returning aid, hcid), addr as (update address set address=$13 where aid=(select aid from per_info)), health as (update healthcare set hcname=$14, hcnum=$15 where hcid=(select hcid from per_info)) insert into condition (diagid, cname, severity) values ';
+  var queryString;
+  if(numberOfCon==0)
+  {
+    queryString = 'with pat as (update patient set pfirst=$1, plast=$2, ssn=$3 where qrcode=$4 returning pid), per_info as (update personal_info set email=$5, marital=$6, gender=$7, weight=$8, height=$9, blood=$10, age=$11, phone=$12 where pid=(select pid from pat) returning aid, hcid), addr as (update address set address=$13 where aid=(select aid from per_info)) update healthcare set hcname=$14, hcnum=$15 where hcid=(select hcid from per_info)';
+  }
+  else {
+    queryString = 'with pat as (update patient set pfirst=$1, plast=$2, ssn=$3 where qrcode=$4 returning pid), per_info as (update personal_info set email=$5, marital=$6, gender=$7, weight=$8, height=$9, blood=$10, age=$11, phone=$12 where pid=(select pid from pat) returning aid, hcid), addr as (update address set address=$13 where aid=(select aid from per_info)), health as (update healthcare set hcname=$14, hcnum=$15 where hcid=(select hcid from per_info)) insert into condition (diagid, cname, severity) values ';
+  }
   //((select diagid from patient, visits, diagnostic where patient.pid=(select pid from pat)), $16, 'High')';
 
   var listOfElements = [patientData.pf, patientData.pl, patientData.ssn, patientData.qr, personalData.email, personalData.marital, personalData.gender, personalData.weight, personalData.height, personalData.blood, personalData.age, personalData.phone, addressData.address, healthData.hcname, healthData.hcnum];
@@ -225,25 +232,25 @@ app.put('/update/patient/', function(request, response) {
       }
       else
       {
-        response.json({Process: 'Complete', Status: 'Succesful'});
+        response.json({Process: 'Complete', Delete_Duplicates: 'Incomplete', Status: 'Succesful'});
         console.log(result.rows);
       }
     });
 
-    console.log('Delete duplicates');
-    client.query('delete from condition where exists (select 1 from condition as t2 where t2.cname = condition.cname and t2.diagid = condition.diagid and t2.cid > condition.cid)', function(err, result) {
-
-      if(err) {
-        done();
-        console.log(err);
-        return response.status(500).json({success: false, data: err});
-      }
-      else
-      {
-        response.json({Delete_Duplicates: 'Complete', Status: 'Succesful'});
-        console.log(result.rows);
-      }
-    });
+    // console.log('Delete duplicates');
+    // client.query('delete from condition where exists (select 1 from condition as t2 where t2.cname = condition.cname and t2.diagid = condition.diagid and t2.cid > condition.cid)', function(err, result) {
+    //
+    //   if(err) {
+    //     done();
+    //     console.log(err);
+    //     return response.status(500).json({success: false, data: err});
+    //   }
+    //   else
+    //   {
+    //     response.json({Process: 'Complete', Delete_Duplicates: 'Complete', Status: 'Succesful'});
+    //     console.log(result.rows);
+    //   }
+    // });
   });
 });
 //_______________________________________________________________________________________________________________________
